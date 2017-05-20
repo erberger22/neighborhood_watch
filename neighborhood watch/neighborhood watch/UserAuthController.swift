377bbf31c1b4eeed8add6279c23950dd91fdc5f1
erberger22 <erberger22@gmail.com>
2017-05-20 16:57:07 -0700

    Add email and password to userauth view. Link view to controller

--- neighborhood watch/neighborhood watch/UserAuthController.swift
+++ neighborhood watch/neighborhood watch/UserAuthController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  UserAuthController.swift
 //  neighborhood watch
 //
 //  Created by Eli Berger on 5/20/17.
@@ -8,21 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class UserAuthController: UIViewController {
     
     
+    @IBOutlet var emailText: UITextField!
+    @IBOutlet var segmentControl: UISegmentedControl!
+    @IBOutlet var passwordText: UITextField!
+    @IBOutlet var actionButton: UIButton!
+    
+    
+    @IBAction func action(_ sender: Any) {
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
