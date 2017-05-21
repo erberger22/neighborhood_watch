610e14877968b51c95a97a23cf66cb658650007c
Harini Balakrishnan <hariniemmawatson@gmail.com>
2017-05-21 15:14:42 -0700

    Add view for the create pin page

---
+++ neighborhood watch/neighborhood watch/CreatePinViewController.swift
@@ -0,0 +1,26 @@
+//
+//  CreatePinViewController.swift
+//  neighborhood watch
+//
+//  Created by Harini Balakrishnan on 5/21/17.
+//
+//
+
+import UIKit
+
+class CreatePinViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+}
