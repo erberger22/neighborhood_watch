8c372f64d803d2b5a6bacc82b52e5b6dc48f34b6
Harini Balakrishnan <hariniemmawatson@gmail.com>
2017-05-21 15:36:41 -0700

    Add picker data and link it to the view page

--- neighborhood watch/neighborhood watch/CreatePinViewController.swift
+++ neighborhood watch/neighborhood watch/CreatePinViewController.swift
@@ -8,8 +8,27 @@
 
 import UIKit
 
-class CreatePinViewController: UIViewController {
+class CreatePinViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    let categories = ["Sanitation","Suspicious Activity","Infrastructure","Crowd","Blocked Roads"]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return  categories[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return categories.count
+    }
+    //var child = ""
+    
+    //func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        //child = categories[row]
+    //}
     override func viewDidLoad() {
         super.viewDidLoad()
 
