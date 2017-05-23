300f0377aed36ae6f2202e056dcacde10dd026fc
Harini Balakrishnan <hariniemmawatson@gmail.com>
2017-05-23 13:16:59 -0700

    Add automatic reload of map and remove old PINs

--- neighborhood watch/neighborhood watch/ViewController.swift
+++ neighborhood watch/neighborhood watch/ViewController.swift
@@ -22,6 +22,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate{
     
     var ref: DatabaseReference!
     var refHandle: UInt!
+    var timer: Timer!
+    var refresher: UIRefreshControl!
     let categories = ["Blocked Roads", "Crowd", "Infrastructure", "Sanitation", "Suspicious Activity"]
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
@@ -43,36 +45,43 @@ class ViewController: UIViewController, CLLocationManagerDelegate{
         let components: DateComponents = calender.dateComponents([.minute, .day, .hour], from: date2, to: date1)
         print(components)
         //if components.hour! >= 6
-        if components.minute! >= 15{
+        if (components.minute! >= 1 || components.day! >= 1 || components.hour! >= 1){
             return false
         }
         return true
     }
-
+    
+    func update() {
+        // Get data from the firebase
+        self.mapView.removeAnnotations(self.mapView.annotations)
+        ref = Database.database().reference()
+        for items in categories {
+            ref.child(items).observe(.childAdded, with: { (snapshot) in
+                let enumerator = snapshot.children
+                var array: [Any] = []
+                while let rest = enumerator.nextObject() as? DataSnapshot {
+                    array.append(rest.value!)
+                }
+                print(array)
+                let pinDescription = array[0]
+                let pinLongitude = array[3]
+                let pinLatitude = array[2]
+                let pinTimeStamp = array[4]
+                let newPin = Location(title: items, locationName: pinDescription as! String, discipline: "discipline", coordinate: CLLocationCoordinate2D(latitude: pinLatitude as! CLLocationDegrees, longitude: pinLongitude as! CLLocationDegrees))
+                //print(self.checkPinStatus(inputTimestamp: pinTimeStamp as! TimeInterval))
+                if (self.checkPinStatus(inputTimestamp: pinTimeStamp as! TimeInterval)){
+                    self.mapView.addAnnotation(newPin)
+                }
+            }, withCancel: nil)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        ref = Database.database().reference()
-        for items in categories {
-        ref.child(items).observe(.childAdded, with: { (snapshot) in
-            let enumerator = snapshot.children
-            var array: [Any] = []
-            while let rest = enumerator.nextObject() as? DataSnapshot {
-                        array.append(rest.value!)
-                        }
-//          print(snapshot)
-            print(array)
-            let pinDescription = array[0]
-            let pinLongitude = array[3]
-            let pinLatitude = array[2]
-            let pinTimeStamp = array[4]
-            
-            if (self.checkPinStatus(inputTimestamp: pinTimeStamp as! TimeInterval)){
-              let newPin = Location(title: items, locationName: pinDescription as! String, discipline: "discipline", coordinate: CLLocationCoordinate2D(latitude: pinLatitude as! CLLocationDegrees, longitude: pinLongitude as! CLLocationDegrees))
-              self.mapView.addAnnotation(newPin)
-            }
-          }, withCancel: nil)
-        }
+        
+        // Refreshing the page every 15 seconds
+        Timer.scheduledTimer(timeInterval: 15, target: self, selector: #selector(self.update), userInfo: nil, repeats: true)
+        
         mapView.delegate = self
         manager.delegate = self
         manager.desiredAccuracy = kCLLocationAccuracyBest
